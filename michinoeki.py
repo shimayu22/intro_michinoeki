fe3768a8f15199522c8c21ce4362f9baeb880476
Yutaka Shimada <shimayu22@gmail.com>
2020-09-09 16:10:50 +0900

    jsonファイルを読み込む

---
+++ michinoeki.py
@@ -0,0 +1,6 @@
+import json
+from pprint import pprint
+import pandas as pd
+
+with open(r"C:\Users\113414A009MEH\Documents\dev\intro_michinoeki\Roadside_Stationhokkaido.json", encoding="utf-8") as f:
+    print(f.read())
