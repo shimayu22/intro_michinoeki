5f8a0e14b67436b03d81a08ecaf5b9549b3f40be
Yutaka Shimada <shimayu22@gmail.com>
2020-09-10 13:10:41 +0900

    道の駅名を表示する

--- michinoeki.py
+++ michinoeki.py
@@ -2,5 +2,13 @@ import json
 from pprint import pprint
 import pandas as pd
 
+
 with open(r"C:\Users\113414A009MEH\Documents\dev\intro_michinoeki\Roadside_Stationhokkaido.json", encoding="utf-8") as f:
-    print(f.read())
+    s = f.read()
+
+jsondata = json.loads(s)
+
+for data in jsondata["features"]:
+    print(data["properties"]["P35_006"])
+
+print(len(jsondata["features"]))
