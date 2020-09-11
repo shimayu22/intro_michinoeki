b102b95dc6b6ff82503e78c1db595117b57eb6de
Yutaka Shimada <shimayu22@gmail.com>
2020-09-11 14:49:56 +0900

    中心座標とズームを調整

--- michinoeki_zenkoku.py
+++ michinoeki_zenkoku.py
@@ -3,14 +3,13 @@ from pprint import pprint
 import pandas as pd
 import folium
 
-m = folium.Map(location=[43.06417, 141.34694], zoom_start=6)
+m = folium.Map(location=[35.6809591, 139.7673068], zoom_start=5)
 
 df = pd.read_json(
     r"C:\Users\113414A009MEH\Documents\dev\intro_michinoeki\P35-18_Roadside_Station.json",
     encoding="utf-8")
 
 df_f = df["features"]
-pprint(df_f)
 
 for data in df_f:
     d = data["properties"]
